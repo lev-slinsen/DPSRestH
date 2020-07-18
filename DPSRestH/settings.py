af4b17e520e7946b34fb54c227e8f0a72ca6fdf3
Ice Ivanov <ice.ivanov@gmail.com>
2020-07-18 13:55:55 +0300

    initial commit

--- DPSRestH/settings.py
+++ DPSRestH/settings.py
@@ -76,17 +76,16 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    # 'https://localhost:8000',
-    # 'http://localhost:8000',
-    # 'http://localhost:3000',
-    # 'http://localhost:3001',
-    # 'http://localhost',
-    # 'https://localhost',
-    # 'https://127.0.0.1:8000',
-    # 'https://127.0.0.1:3000',
-    # 'http://127.0.0.1:8000',
-    # 'http://127.0.0.1:3000',
-    '*'
+    'https://localhost:8000',
+    'http://localhost:8000',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost',
+    'https://localhost',
+    'https://127.0.0.1:8000',
+    'https://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
 ]
 
 CSRF_COOKIE_NAME = "csrftoken"
