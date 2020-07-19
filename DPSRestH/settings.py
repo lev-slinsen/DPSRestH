f624f57abd6496529bc19ff8a33df7b75f7cd120
Ice Ivanov <ice.ivanov@gmail.com>
2020-07-19 16:38:15 +0300

    trying out google storage for images

--- DPSRestH/settings.py
+++ DPSRestH/settings.py
@@ -154,8 +154,8 @@ LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale'),
 )
 
-GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = os.getenv('GS_CREDENTIALS')
-GS_CREDENTIALS = os.getenv('GS_CREDENTIALS')
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = 'google-secret.json'
+# GS_CREDENTIALS = os.getenv('GS_CREDENTIALS')
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'pechorin-bucket'
 STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
