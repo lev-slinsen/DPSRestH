15a467ca0e67943f13007a11b3c30974c688ba47
Ice Ivanov <ice.ivanov@gmail.com>
2020-09-03 00:29:04 +0300

    updated cors headers for github

--- DPSRestH/settings.py
+++ DPSRestH/settings.py
@@ -84,6 +84,7 @@ CORS_ORIGIN_WHITELIST = [
     'https://127.0.0.1:3000',
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
+    'https://ice-ivanov.github.io/DPSRest',
 ]
 
 CSRF_COOKIE_NAME = "csrftoken"
