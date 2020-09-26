82fa8d3b7a2967ad0b0e74a2b2311b6dd23ed80a
Ice Ivanov <ice.ivanov@gmail.com>
2020-09-26 14:09:41 +0300

    added cors headers http://pechori.fps.by

--- DPSRestH/settings.py
+++ DPSRestH/settings.py
@@ -85,6 +85,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
     'https://ice-ivanov.github.io/DPSRest',
+    'http://pechori.fps.by',
 ]
 
 CSRF_COOKIE_NAME = "csrftoken"
